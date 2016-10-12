6a1f3c0834a5e215120bd6b2c9dac089e0e057c4
Ted Chen <ted.chen@emc.com>
2016-10-12 18:04:59 +0000

    First commit, not working yet

---
+++ app.js
@@ -0,0 +1,66 @@
+// Copyright 2016, EMC, Inc.
+/* jshint node: true */
+
+"use strict";
+
+var di = require('di');
+
+module.exports = Runner;
+
+di.annotate(Runner, new di.Provide('app'));
+di.annotate(Runner, new di.Inject(
+    'Http.Server',
+    'Services.Configuration',
+    'fileService',
+    'Logger',
+    'Promise'
+)
+);
+function Runner(
+    HttpService,
+    configuration,
+    fileService,
+    Logger,
+    Promise
+) {
+    var logger = Logger.initialize("app");
+
+    var services = [];
+
+    var defaultEndpoints = [
+        {
+            "address": "0.0.0.0",
+            "port": 8080,
+            "routers": "northbound"
+        },
+        {
+            "address": "0.0.0.0",
+            "port": 9080,
+            "routers": "southbound"
+        }
+    ];
+
+    function start() {
+        return Promise.resolve()
+            .then(function () {
+                logger.info('debug, in service start');
+                var endpoints = configuration.get('httpEndpoints', defaultEndpoints);
+                services = Promise.map(endpoints, function (endpoint) {
+                    return new HttpService(endpoint);
+                })
+
+                return services;
+            });
+    }
+
+    function stop() {
+        return Promise.map(services, function (service) {
+            return service.stop();
+        })
+    }
+
+    return {
+        start: start,
+        stop: stop
+    };
+}
---
+++ index.js
@@ -0,0 +1,69 @@
+// Copyright 2016, EMC, Inc.
+
+'use strict';
+
+var _ = require('lodash');
+var di = require('di');
+
+if (require.main === module) { run(); }
+
+function run() {
+    // setup dependency injection
+    var helper = require('./lib/di')(di, __dirname);
+    var injectables = _.flattenDeep(
+        [
+            // NPM packages
+            helper.simpleWrapper(_, '_'),
+            helper.requireWrapper('nconf'),
+            helper.requireWrapper('assert-plus', 'assert'),
+            helper.requireWrapper('fs', 'fs'),
+            helper.requireWrapper('path', 'path'),
+            helper.requireWrapper('child_process', 'child_process'),
+            helper.requireWrapper('bluebird', 'Promise'),
+            helper.requireWrapper('express', 'express'),
+            helper.requireWrapper('serve-index', 'serve-index'),
+            require('./app'),
+
+            // Glob Requirables
+            helper.requireGlob(__dirname + '/lib/*.js')
+        ]
+    );
+
+    var injector = new di.Injector(injectables);
+
+    // setup app runner
+    var app = injector.get('app');
+    var Logger = injector.get('Logger');
+    var logger = Logger.initialize("main");
+
+
+
+    app.start()
+        .then(function () {
+            logger.info('Server Started.');
+        })
+        .catch(function (error) {
+            logger.critical('Server Startup Error.', { error: error });
+
+            process.nextTick(function () {
+                process.exit(1);
+            });
+        });
+
+    process.on('SIGINT', function () {
+        app.stop()
+            .catch(function (error) {
+                logger.critical('Server Shutdown Error.', { error: error });
+            })
+            .finally(function () {
+                process.nextTick(function () {
+                    process.exit(1);
+                });
+            });
+    });
+}
+
+// app.use(express.compress());
+// app.use('/static', express.static('/home/onrack/test_ng/'));
+// app.use('/static', directory('/home/onrack/', {'icons': true}));
+// app.listen(process.env.PORT || 9998);
