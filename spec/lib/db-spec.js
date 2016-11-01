4bbf72be13d9faec98ab189341460a5426bba2a2
Felix Yuan <felix.yuan@emc.com>
2016-11-01 18:19:06 +0000

    ading promise

--- spec/lib/db-spec.js
+++ spec/lib/db-spec.js
@@ -4,11 +4,9 @@
 'use strict';
 
 describe('database service', function () {
-    var spyLoad, spyGetAllImages, spyFindOneImage, spyUpdateImage;
     var sandbox = sinon.sandbox.create();
     var database;
     var lowdb;
-    var spyLowdb;
 
     var testData = {
         "images": [
@@ -36,11 +34,6 @@ describe('database service', function () {
         setConfig();
         database = helper.injector.get('Services.Database');
         lowdb = helper.injector.get('lowdb');
-        spyLoad = sandbox.spy(database, 'load');
-        spyGetAllImages = sandbox.spy(database, 'getAllImages');
-        spyFindOneImage = sandbox.spy(database, 'findOneImage');
-        spyUpdateImage = sandbox.spy(database, 'updateImage');
-        spyLowdb = sandbox.spy(lowdb);
     });
 
     after('restore config', function () {
@@ -64,11 +57,10 @@ describe('database service', function () {
     describe("test database service", function () {
 
         it("should return all database", function () {
-
-            var ret= database.load().getAllImages();
-            expect(spyGetAllImages).to.be.calledOnce
-                .and.returned(allImages);
-            console.log(ret);
+            return database.load(function(){
+                return database.getAllImages()
+                    .should.eventually.deep.equal(allImages);
+            });
         });
 
         it("should return one queried images", function () {
@@ -77,9 +69,22 @@ describe('database service', function () {
                 version: '7.0'
             };
 
-            database.findOneImage(query);
-            expect(spyFindOneImage).to.be.calledOnce
-                .and.returned(image);
+            return database.findOneImage(query)
+                .then(function(){
+                    return database.findOneImage(query)
+                        .should.eventually.deep.equal(image);
+                });
+        });
+
+        it("should return one queried images with name and version", function () {
+            var name = 'centos',
+                version = '7.0';
+
+            return database.findOneImageByNameVersion(name, version)
+                .then(function(){
+                    return database.findOneImage({name: name, version: version})
+                        .should.eventually.deep.equal(image);
+                });
         });
 
         it("should update selected image", function () {
@@ -88,23 +93,66 @@ describe('database service', function () {
                 version: '7.0'
             };
 
-            var data = {
-                status: 'Failed'
-            };
-
-            var returnImage = {
+            var data = {status: "Failed"};
+            var updatedImage = {
                 "id": "b6b3e3be-c799-4af4-86c8-09a99d3aa7c7",
                 "iso": "centos-7.0.iso",
                 "name": "centos",
                 "version": "7.0",
                 "status": "Failed"
             };
-            database.updateImage(query, data);
-            expect(spyUpdateImage).to.be.calledOnce
-                .and.returned(returnImage);
 
-            // restore test data
-            database.updateImage(query, image);
+            database.updateImage(query, data)
+                .then(function(){
+                    return database.findOneImage(query)
+                        .should.eventually.deep.equal(updatedImage);
+                })
+                .then(function(){
+                    // restore test data
+                    return database.updateImage(query, image);
+                });
+        });
+
+        it("should update status of selected image", function () {
+            var name = 'centos',
+                version = '7.0';
+
+            var status = 'Failed';
+
+            var updatedImage = {
+                "id": "b6b3e3be-c799-4af4-86c8-09a99d3aa7c7",
+                "iso": "centos-7.0.iso",
+                "name": "centos",
+                "version": "7.0",
+                "status": "Failed"
+            };
+
+            database.updateImageStatus(name, version, status)
+                .then(function(){
+                    return database.findOneImageByNameVersion(name, version)
+                        .should.eventually.deep.equal(updatedImage);
+                })
+                .then(function(){
+                    // restore test data
+                    return database.updateImageStatus(name, version, image);
+                });
+        });
+
+        it("should delete selected image", function () {
+            var query = {
+                name: 'centos',
+                version: '7.0'
+            };
+
+            return database.deleteImage(query)
+                .then(function(){
+                    return database.findOneImage(query)
+                        .should.eventually.deep.equal();
+                })
+                .then(function(){
+                    // restore test data
+                    return database.addImage(image);
+                });
         });
 
     });
